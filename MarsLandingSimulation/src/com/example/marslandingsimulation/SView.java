9e1a546b47b16e3b67a96346c9fb9fbcfa2969e3
ismail <ismail@Extreme-Pc>
2013-10-20 12:38:52 +1300

    Finished commenting the SView methods

--- MarsLandingSimulation/src/com/example/marslandingsimulation/SView.java
+++ MarsLandingSimulation/src/com/example/marslandingsimulation/SView.java
@@ -2,7 +2,6 @@ package com.example.marslandingsimulation;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -27,6 +26,15 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
+/**
+ * SView Surface View.
+ * Detect when the space craft intersects with the martian landscape.
+ * Scale the Terrain, spaceship and movement that will work with other resolutions.
+ * Setup Accelerometer to control the space craft’s main, left and right thrusters
+ * Setup sounds effects.
+ * setup an event handler to handle onTouch
+ * handle the physics
+ */
 public class SView extends SurfaceView implements Runnable,
 		SurfaceHolder.Callback, OnTouchListener, SensorEventListener {
 	
@@ -38,7 +46,7 @@ public class SView extends SurfaceView implements Runnable,
 	static final int REFRESH_RATE = 5;
 	static final int GRAVITY = 4;
 	static final int MOVEMENT = 4;
-	static final int fuelUsage = 1;
+	static final int fuelUsage = 2;
 	double t = 0.1;
 	static final int maxFuel = 800;
 	int fuel = maxFuel;
@@ -136,13 +144,64 @@ public class SView extends SurfaceView implements Runnable,
 		super(context, attrs, defStyle);
 		init();
 	}
-
-
+	
+	/**
+	 * Initialises ontouchlistener and holder.
+	 */
 	public void init() {
 		setOnTouchListener(this);
 		getHolder().addCallback(this);
 	}
- 
+	
+	/**
+	 * Scale display from current testing device (S4)
+	 * Add points for landing area
+	 * Add points for the martian landscape
+	 * Draw path for martian landscape
+	 */
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		// Scale display from current testing device (S4)
+		sX = 1920 / (float) w;
+		sY = 1080 / (float) h;
+		//Add points for landing area
+		xcorland.add((int) (1140 / sX));	ycorland.add((int) (1005 / sY));
+		xcorland.add((int) (1140 / sX));	ycorland.add((int) (1030 / sY));
+		xcorland.add((int) (1340 / sX));	ycorland.add((int) (1030 / sY));
+		xcorland.add((int) (1340 / sX));	ycorland.add((int) (1005 / sY));
+		xcorland.add((int) (1140 / sX));	ycorland.add((int) (1005 / sY));
+		//Add points for the martian landscape
+		xcor.add(0);					ycor.add((int) (980 / sY));
+		xcor.add((int) (80 / sX)); 		ycor.add((int) (880 / sY));
+		xcor.add((int) (190 / sX));		ycor.add((int) (600 / sY));
+		xcor.add((int) (300 / sX));		ycor.add((int) (700 / sY));
+		xcor.add((int) (570 / sX));		ycor.add((int) (780 / sY));
+		xcor.add((int) (710 / sX));		ycor.add((int) (560 / sY));
+		xcor.add((int) (890 / sX));		ycor.add((int) (430 / sY));
+		xcor.add((int) (1090 / sX)); 	ycor.add((int) (470 / sY));
+		xcor.add((int) (1210 / sX)); 	ycor.add((int) (720 / sY));
+		xcor.add((int) (1140/ sX)); 	ycor.add((int) (940 / sY));
+		xcor.add((int) (1140 / sX)); 	ycor.add((int) (1030 / sY));
+		xcor.add((int) (1340 / sX)); 	ycor.add((int) (1030 / sY));
+		xcor.add((int) (1420 / sX)); 	ycor.add((int) (930 / sY));
+		xcor.add((int) (1590 / sX)); 	ycor.add((int) (840 / sY));
+		xcor.add((int) (1710 / sX)); 	ycor.add((int) (590 / sY));
+		xcor.add((int) (1810 / sX)); 	ycor.add((int) (490 / sY));
+		xcor.add(width); 				ycor.add((int) (520 / sY));
+		xcor.add(width); 				ycor.add(height);
+		xcor.add(0); 					ycor.add(height);
+		xcor.add(0); 					ycor.add((int) (980 / sY));
+		// Draw path for martian landscape
+		path = new Path();
+		for (int i = 0; i < xcor.size(); i++) {
+			path.lineTo(xcor.get(i), ycor.get(i));
+		}
+	}
+	
+	/**
+	 * Using OnTouch method to confirm the player
+	 */
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		// TODO Auto-generated method stub
@@ -152,46 +211,9 @@ public class SView extends SurfaceView implements Runnable,
 		return true;
 	}
 	
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		super.onSizeChanged(w, h, oldw, oldh);
-		
-		sX = 1920 / (float) w;
-		sY = 1080 / (float) h;
-		
-		xcorland.add((int) (1140 / sX));	ycorland.add((int) (1005 / sY));
-		xcorland.add((int) (1140 / sX));	ycorland.add((int) (1030 / sY));
-		xcorland.add((int) (1340 / sX));	ycorland.add((int) (1030 / sY));
-		xcorland.add((int) (1340 / sX));	ycorland.add((int) (1005 / sY));
-		xcorland.add((int) (1140 / sX));	ycorland.add((int) (1005 / sY));
-		
-		xcor.add(0);	ycor.add((int) (980 / sY));
-		xcor.add((int) (80 / sX));	ycor.add((int) (880 / sY));
-		xcor.add((int) (190 / sX));	ycor.add((int) (600 / sY));
-		xcor.add((int) (300 / sX));	ycor.add((int) (700 / sY));
-		xcor.add((int) (570 / sX));	ycor.add((int) (780 / sY));
-		xcor.add((int) (710 / sX));	ycor.add((int) (560 / sY));
-		xcor.add((int) (890 / sX));	ycor.add((int) (430 / sY));
-		xcor.add((int) (1090 / sX)); ycor.add((int) (470 / sY));
-		xcor.add((int) (1210 / sX)); ycor.add((int) (720 / sY));
-		xcor.add((int) (1140/ sX)); ycor.add((int) (940 / sY));
-		xcor.add((int) (1140 / sX)); ycor.add((int) (1030 / sY));
-		xcor.add((int) (1340 / sX)); ycor.add((int) (1030 / sY));
-		xcor.add((int) (1420 / sX)); ycor.add((int) (930 / sY));
-		xcor.add((int) (1590 / sX)); ycor.add((int) (840 / sY));
-		xcor.add((int) (1710 / sX)); ycor.add((int) (590 / sY));
-		xcor.add((int) (1810 / sX)); ycor.add((int) (490 / sY));
-		xcor.add(width); ycor.add((int) (520 / sY));
-		xcor.add(width); ycor.add(height);
-		xcor.add(0); ycor.add(height);
-		xcor.add(0); ycor.add((int) (980 / sY));
-		// Draw path
-		path = new Path();
-		for (int i = 0; i < xcor.size(); i++) {
-			path.lineTo(xcor.get(i), ycor.get(i));
-		}
-	}
-
+	/**
+	 * Main thread for SView to draw onto the canvas
+	 */
 	@Override
 	public void run() {
 		while (!Thread.interrupted()) {
@@ -213,13 +235,14 @@ public class SView extends SurfaceView implements Runnable,
 					canvas.drawPath(path, paint1);
 					// Fuel
 					Paint paintFuel = new Paint();
-					paintFuel.setColor(Color.GRAY);
+					paintFuel.setColor(Color.DKGRAY);
 					canvas.drawRect(0, (float)((height)-(5/sY)), (float)((width/2)+(5/sY)), (float)((height)-(35/sY)), paintFuel);
 					paint.setColor(Color.CYAN);
 					canvas.drawRect(0, (float)((height)-(10/sY)), fuel * (float)(width/2) / maxFuel, (float) ((height)-(30/sY)), paint);
 					paint.setTextSize((float)(40/sY));
 					canvas.drawText("Fuel:", 0, (float)((height)-(40/sY)), paint);
 					if (!gameover) {
+						// Allow the player to move right if theres fuel.
 						if (yAxis > 2 && yAxis < 8 && !fuelFinished) {
 							x = x + (MOVEMENT/sX);
 							y = y - 1;
@@ -227,27 +250,32 @@ public class SView extends SurfaceView implements Runnable,
 							canvas.drawBitmap(sLeft, x - (65/sX), y + (28/sY), paint);
 							fuel -= fuelUsage;
 							mp.start();
-						} else if (yAxis < -2 && yAxis > -8 && !fuelFinished) {
+						} 
+						// Allow the player to move left if theres fuel.
+						else if (yAxis < -2 && yAxis > -8 && !fuelFinished) {
 							x = x - (MOVEMENT/sX);
 							y = y - (1/sY);
 							t = 1.1;
 							canvas.drawBitmap(sRight, x + (35/sX), y + (28/sY), paint);
 							fuel -= fuelUsage;
 							mp.start(); 
-						} else if (xAxis < 8 && xAxis > 2 && !fuelFinished) {
+						} 
+						// Allow the player to move upward if theres fuel.
+						else if (xAxis < 8 && xAxis > 2 && !fuelFinished) {
 							y = y - (4/sY);
 							t = 0.8;
 							canvas.drawBitmap(sMain, x - (25/sX), y + (35/sY), paint);
 							fuel -= (fuelUsage + 1);
 							mp.start();
-						} else {
+						} 
+						// Make the player space ship falls due to gravity.
+						else {
 							y = (int) y + (int)(t + (0.5 * ((GRAVITY/sY) * t * t)));
 							t = t + 0.01;
 							mp.pause();
 						}
 					}
-
-					// If object hit the side
+					// If ship hit the side
 					if (x < 0) {
 						x = width;
 					}
@@ -265,12 +293,13 @@ public class SView extends SurfaceView implements Runnable,
 					fuelFinished = true;
 				}
 				canvas.drawBitmap(landing, xcorland.get(0), ycorland.get(0), paint);
+				// Check if the player landed safely
 				if (contains(xcorland, ycorland, x - (60/sX), y + (30/sY)) || contains(xcorland, ycorland, x + (60/sX), y + (30/sY))) {
 					gameover = true;
 					mp.pause();
 					canvas.drawBitmap(sMessage2,  (float)((width/2)-(960/sX)), (float)((height/2)-(540/sX)), paint);
 				}
-
+				// Check if the player interact with the Terrain
 				if (contains(xcor, ycor, x - (55/sX), y + (25/sY)) || contains(xcor, ycor, x + (55/sX), y + (25/sY)) || contains(xcor, ycor, x, y + (25/sY)) 
 						|| contains(xcor, ycor, x - (28/sX), y + (25/sY))|| contains(xcor, ycor, x + (28/sX), y + (25/sY))) {
 					long current = android.os.SystemClock.uptimeMillis();
@@ -279,10 +308,11 @@ public class SView extends SurfaceView implements Runnable,
 						sP.play(explosion1, 1, 1, 1, 0, 1f);
 						animStart = 1;
 					}
-					
 					int gifDuration = explodeGif.duration();
 					if (gifDuration == 0)
+					{
 						gifDuration = 1000;
+					}
 					
 					int time = (int) ((current - animStart) % gifDuration);
 					explodeGif.setTime(time);
@@ -320,29 +350,27 @@ public class SView extends SurfaceView implements Runnable,
 
 	}
 
+	/**
+	 * Checks if the given path contains the x and y of the space ship
+	 * @param xcor,ycor,x0,y0
+	 * @return even or odd
+	 */
 	public boolean contains(ArrayList<Integer> xcor, ArrayList<Integer> ycor, double x0, double y0) {
 		int crossings = 0;
-
 		for (int i = 0; i < xcor.size() - 1; i++) {
 			int x1 = xcor.get(i);
 			int x2 = xcor.get(i + 1);
-
 			int y1 = ycor.get(i);
 			int y2 = ycor.get(i + 1);
-
 			int dy = y2 - y1;
 			int dx = x2 - x1;
-
 			double slope = 0;
 			if (dx != 0) {
 				slope = (double) dy / dx;
 			}
-
-			boolean cond1 = (x1 <= x0) && (x0 < x2); // is it in the range?
-			boolean cond2 = (x2 <= x0) && (x0 < x1); // is it in the reverse
-														// range?
-			boolean above = (y0 < slope * (x0 - x1) + y1); // point slope y - y1
-
+			boolean cond1 = (x1 <= x0) && (x0 < x2);
+			boolean cond2 = (x2 <= x0) && (x0 < x1);
+			boolean above = (y0 < slope * (x0 - x1) + y1);
 			if ((cond1 || cond2) && above) {
 				crossings++;
 			}
@@ -353,15 +381,20 @@ public class SView extends SurfaceView implements Runnable,
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		// TODO Auto-generated method stub
-
 	}
 
+	/**
+	 * Get the sensor X axis and Y axis
+	 */
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		xAxis = event.values[0];
 		yAxis = event.values[1];
 	}
 
+	/**
+	 * Reset the game
+	 */
 	public void reset() {
 		t = 0.1;
 		fuel = maxFuel;
@@ -380,6 +413,10 @@ public class SView extends SurfaceView implements Runnable,
 		// TODO Auto-generated method stub
 	}
 
+	/**
+	 * Start new thread
+	 * Set Pause to false
+	 */
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		main = new Thread(this);
@@ -387,12 +424,23 @@ public class SView extends SurfaceView implements Runnable,
 			main.start();
 		pause = false;
 	}
+	
+	/**
+	 * Pause the game
+	 * Pause the sounds
+	 * Unregister the listener for the sensor
+	 */
 	public void pause()
 	{
 		pause = true;
 		mp.pause();
 		newSensor.unregisterListener(this);
 	}
+	
+	/**
+	 * Resume Game, sounds
+	 * Register the listener for the sensor
+	 */
 	public void resume(){
 		mp.start();
 		newSensor.registerListener(this, newSensor.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
